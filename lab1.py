bf3863b65baf24c335ea01214e5aba386a8d176b
luciasalar <luciasalar@gmail.com>
2018-09-26 01:20:23 +0100

    lab one

---
+++ lab1.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import pandas as pd 
+
+read 
+
+
+def preprocess(sent):
+
+    words = str(sent).split()
+    new_words = []
+    # ps = PorterStemmer()
+    
+    for w in words:
+        w = w.lower()
+        # remove non English word characters
+        w = re.sub(r'[^\x00-\x7F]+',' ', w)
+        # remove puncutation 
+        w = re.sub(r'[^\w\s]','',w)
+        # w = ps.stem(w)
+        new_words.append(w)
+        
+    return ' '.join(new_words)
+
